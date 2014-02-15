afb1d5007f6ce63f9920a05a32168cddf005ab6f
Dmitry Vasiliev <dima@hlabs.org>
2014-02-15 19:06:01 +0100

    Collect and print more information

--- src/stats/mod.rs
+++ src/stats/mod.rs
@@ -12,31 +12,54 @@ struct ObjectStats {
     sent_bytes: uint
 }
 
-pub type StatsKeyValue<'r> = (&'r ~str, &'r ObjectStats);
+type StatsItem<'r, T> = (&'r T, &'r ObjectStats);
+type StatsMap<T> = HashMap<T, ObjectStats>;
 
 pub struct LogStats {
-    priv clients: HashMap<~str, ObjectStats>,
+    priv clients: StatsMap<~str>,
+    priv methods: StatsMap<~str>,
+    priv paths: StatsMap<~str>,
+    priv statuses: StatsMap<u16>,
+    priv referers: StatsMap<~str>,
+    priv user_agents: StatsMap<~str>,
 }
 
 impl LogStats {
     pub fn new() -> ~LogStats {
-        ~LogStats{clients: HashMap::new()}
+        ~LogStats{
+            clients: HashMap::new(),
+            methods: HashMap::new(),
+            paths: HashMap::new(),
+            statuses: HashMap::new(),
+            referers: HashMap::new(),
+            user_agents: HashMap::new(),
+            }
     }
 }
 
 impl LogProcessor for LogStats {
     fn process(&mut self, record: HTTPLogRecord) {
-        self.clients.insert_or_update_with(
-            record.remote_addr.into_owned(),
-            ObjectStats{
-                requests: 1,
-                request_time: record.request_time,
-                sent_bytes: record.sent_bytes,
-                },
-            |_, stats| {
-                stats.requests += 1;
-                stats.request_time += record.request_time;
-                stats.sent_bytes += record.sent_bytes;
-            });
+        update(&mut self.clients, record.remote_addr.into_owned(), &record);
+        update(&mut self.methods, record.method.into_owned(), &record);
+        update(&mut self.paths, record.path.into_owned(), &record);
+        update(&mut self.statuses, record.status, &record);
+        update(&mut self.referers, record.referer.into_owned(), &record);
+        update(&mut self.user_agents, record.user_agent.into_owned(), &record);
     }
+}
+
+fn update<T: IterBytes + Eq>(mapping: &mut StatsMap<T>, key: T,
+        record: &HTTPLogRecord) {
+    mapping.insert_or_update_with(
+        key,
+        ObjectStats{
+            requests: 1,
+            request_time: record.request_time,
+            sent_bytes: record.sent_bytes,
+            },
+        |_, stats| {
+            stats.requests += 1;
+            stats.request_time += record.request_time;
+            stats.sent_bytes += record.sent_bytes;
+        });
 }
--- src/stats/printer.rs
+++ src/stats/printer.rs
@@ -1,4 +1,6 @@
-use super::{LogStats, StatsKeyValue};
+use std::fmt::Default;
+
+use super::{LogStats, StatsItem, StatsMap};
 use super::utils::{format_duration, format_bytes};
 
 
@@ -15,27 +17,34 @@ impl<'r> LogStatsPrinter<'r> {
     }
 
     pub fn print(&self) {
-        let mut stats: ~[StatsKeyValue] = self.stats.clients.iter().collect();
-        stats.sort_by(|&(_, a), &(_, b)| b.requests.cmp(&a.requests));
-        print_sorted(stats, "By requests");
-        stats.sort_by(|&(_, a), &(_, b)| b.request_time.cmp(&a.request_time));
-        print_sorted(stats, "By request time");
-        stats.sort_by(|&(_, a), &(_, b)| b.sent_bytes.cmp(&a.sent_bytes));
-        print_sorted(stats, "By sent bytes");
+        print(&self.stats.clients, "by requests", "Clients");
+        print(&self.stats.paths, "by requests", "Paths");
+        print(&self.stats.methods, "by requests", "Methods");
+        print(&self.stats.statuses, "by requests", "Statuses");
+        print(&self.stats.referers, "by requests", "Referers");
+        print(&self.stats.user_agents, "by requests", "User agents");
     }
 }
 
-fn print_sorted(sorted: &[StatsKeyValue], message: &str) {
-    println!("\n  {}", message);
+fn print<T: IterBytes + Eq + Default>(mapping: &StatsMap<T>, title: &str,
+        key_title: &str) {
+    let mut items: ~[StatsItem<T>] = mapping.iter().collect();
+    items.sort_by(|&(_, a), &(_, b)| b.requests.cmp(&a.requests));
+    print_sorted(items, title, key_title);
+}
+
+fn print_sorted<T: IterBytes + Eq + Default>(sorted: &[StatsItem<T>],
+        title: &str, key_title: &str) {
+    println!("\n{} {}", key_title, title);
     println!("=====================================================\
-              =============");
-    println!("Client                                 \
-              Requests   Duration   Bytes");
+              ============================");
+    println!("{: <57} Requests Duration Bytes", key_title);
     println!("-----------------------------------------------------\
-              -------------");
+              ----------------------------");
     for &(client, stats) in sorted.iter().take(NUMBER_OF_ITEMS) {
-        println!("{: <40} {: >6} {: >10} {: >7}",
-                 *client, stats.requests,
+        println!("{: <60.60} {: >5} {: >8} {: >5}",
+                 *client,
+                 stats.requests,
                  format_duration(stats.request_time),
                  format_bytes(stats.sent_bytes));
     }
