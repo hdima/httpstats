4486832b2cd444a1d93e3b8b83c2162f926d09f5
Dmitry Vasiliev <dima@hlabs.org>
2014-09-20 18:23:57 +0200

    Number of items now can be configured

--- src/httpstats.rs
+++ src/httpstats.rs
@@ -1,10 +1,12 @@
 extern crate time;
 extern crate libc;
+extern crate getopts;
 extern crate collections;
 
 use std::os;
 use std::io::util::ChainedReader;
 use std::io::BufferedReader;
+use getopts::{getopts, optopt, usage, OptGroup};
 
 use stats::LogStats;
 use stats::printer::LogStatsPrinter;
@@ -16,9 +18,9 @@ mod stats;
 mod gzreader;
 
 
-static NUMBER_OF_ITEMS_TO_PRINT: uint = 10u;
+static DEFAULT_NUMBER_OF_ITEMS_TO_PRINT: uint = 10u;
 
-fn parse(filenames: &[String]) {
+fn parse(filenames: &[String], n: uint) {
     let files = filenames.iter().map(|filename| {
         let path = Path::new(filename.clone());
         GzipReader::open(&path).unwrap()
@@ -29,16 +31,51 @@ fn parse(filenames: &[String]) {
     let mut parser = NginxLogParser::new(reader);
     parser.parse(&mut stats);
     let printer = LogStatsPrinter::new(&stats);
-    printer.print(NUMBER_OF_ITEMS_TO_PRINT);
+    printer.print(n);
+}
+
+fn print_usage(msg: &str, program: &str, opts: &[OptGroup]) {
+    println!("{}\n\nSimple HTTP statistics, version 0.0.2\n\n\
+              Usage: {} LOGFILE...{}", msg, program, usage("", opts));
+    os::set_exit_status(2);
+}
+
+fn items_to_print(opt: Option<String>) -> Result<uint, String> {
+    match opt {
+        Some(str_n) => {
+            let n_opt: Option<uint> =  from_str(str_n.as_slice());
+            match n_opt {
+                Some(n) if n > 0 => Ok(n),
+                _ => Err(str_n)
+            }
+        },
+        None => Ok(DEFAULT_NUMBER_OF_ITEMS_TO_PRINT)
+    }
 }
 
 fn main() {
     let args = os::args();
-    if args.len() < 2 {
-        println!("Simple HTTP statistics, version 0.0.2\n\n\
-                  Usage: {} LOGFILE...", args[0]);
-        os::set_exit_status(2);
+    let program = args[0].clone();
+    let opts = [
+        optopt("n", "", "number of items to print", "NUMBER")
+    ];
+    let matches = match getopts(args.tail(), opts) {
+        Ok(m) => m,
+        Err(err) => {
+            print_usage(err.to_string().as_slice(), program.as_slice(), opts);
+            return;
+        }
+    };
+    if !matches.free.is_empty() {
+        match items_to_print(matches.opt_str("n")) {
+            Ok(n) => parse(matches.free.as_slice(), n),
+            Err(_str_n) => {
+                print_usage("Invalid number of items provided", program.as_slice(), opts);
+                return;
+            }
+        }
     } else {
-        parse(args.slice_from(1));
+        print_usage("No log files provided", program.as_slice(), opts);
+        return;
     }
 }
