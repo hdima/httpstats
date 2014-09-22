3a9dd2409f640e841e2198ef2b61bf9c34cb3b7f
Dmitry Vasiliev <dima@hlabs.org>
2014-09-22 22:23:42 +0200

    Better error reporting for gzreader

--- src/gzreader.rs
+++ src/gzreader.rs
@@ -1,9 +1,7 @@
-use std::path::Path;
-use std::io::{Reader, IoResult, standard_error, EndOfFile, OtherIoError};
-use std::option::{Some, Option};
-//use std::c_str::CString;
+use std::io::{IoResult, standard_error, EndOfFile, OtherIoError, IoError};
+use std::c_str::CString;
 use std::os::last_os_error;
-use libc::types::os::arch::c95::{c_char, c_int, size_t};
+use libc::{c_int, c_char, c_uint};
 
 
 enum GzFile {}
@@ -11,13 +9,15 @@ enum GzFile {}
 #[link(name="z")]
 extern {
     fn gzopen(filename: *const c_char, mode: *const c_char) -> *const GzFile;
-    fn gzread(file: *const GzFile, buf: *mut u8, len: size_t) -> c_int;
+    fn gzread(file: *const GzFile, buf: *mut u8, len: c_uint) -> c_int;
     fn gzclose(file: *const GzFile) -> c_int;
-    //fn gzerror(file: *GzFile, errnum: *c_int) -> *c_char;
+    fn gzerror(file: *const GzFile, errnum: *mut c_int) -> *const c_char;
 }
 
 pub struct GzipReader {
     file: *const GzFile,
+    path: Path,
+    closed: bool,
 }
 
 impl GzipReader {
@@ -28,7 +28,44 @@ impl GzipReader {
         if file.is_null() {
             fail!("ERROR opening '{}': {}", path.as_str(), last_os_error());
         }
-        Some(GzipReader{file: file})
+        Some(GzipReader{file: file, path: path.clone(), closed: false})
+    }
+
+    fn get_error(&mut self) -> Option<IoError> {
+        unsafe {
+            let mut errnum: c_int = 0;
+            let msg = CString::new(gzerror(self.file, &mut errnum), true);
+            if errnum < 0 {
+                let detail = self.path.as_str().unwrap().into_string()
+                             + msg.as_str().unwrap();
+                Some(IoError{kind: OtherIoError,
+                             desc: "GZip reader error",
+                             detail: Some(detail)
+                            })
+            } else {
+                None
+            }
+        }
+    }
+
+    fn close(&mut self) -> Option<IoError> {
+        if !self.closed {
+            self.closed = true;
+            unsafe {
+                match gzclose(self.file) {
+                    0 => None,
+                    _ => self.get_error()
+                }
+            }
+        } else {
+            None
+        }
+    }
+}
+
+impl Drop for GzipReader {
+    fn drop(&mut self) {
+        self.close();
     }
 }
 
@@ -36,24 +73,18 @@ impl Reader for GzipReader {
     #[inline]
     fn read(&mut self, buf: &mut [u8]) -> IoResult<uint> {
         unsafe {
-            match gzread(self.file, buf.as_mut_ptr(), buf.len() as size_t) {
+            match gzread(self.file, buf.as_mut_ptr(), buf.len() as c_uint) {
                 size if size > 0 => Ok(size as uint),
                 0 => {
-                    // TODO: We can define constants for all the error values
-                    // and check gzclose() errors
-                    gzclose(self.file);
-                    Err(standard_error(EndOfFile))
+                    match self.close() {
+                        None => Err(standard_error(EndOfFile)),
+                        Some(err) => Err(err)
+                    }
                 }
                 _ => {
-                    // FIXME: See below
-                    //let errnum: c_int = 0;
-                    //let s = CString::new(gzerror(self.file, &errnum), true);
-                    // TODO: We can define constants for all the error values
-                    // and check gzclose() errors
-                    // FIXME: See above
-                    //fail!("ERROR reading file: {}", s.as_str().unwrap());
-                    gzclose(self.file);
-                    Err(standard_error(OtherIoError))
+                    let err = self.get_error().unwrap();
+                    self.close();
+                    Err(err)
                 }
             }
         }
